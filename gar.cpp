ffb8fc930b26ee0fca9e7b4d83b966ff0f0c8a11
Christopher Palileo <cpalileo@gmail.com>
2023-12-11 20:08:58 -0800

    Starting Over

--- gar.cpp
+++ gar.cpp
@@ -4,386 +4,113 @@
 #include<ctime>
 using namespace std;
 
-
-struct FitnessData {
-    double averageFitness;
-};
+enum GridState { EMPTY = 0, ROBOT = 'R', BATTERY = 'B', WALL = 'W' };
 
 class Grid {
 private:
-    // Sets up 2D array
-    int GridArray[10][10];
+    GridState GridArray[12][12];
 
 public:
-    // Constructor to initialize all spaces to blank
-    Grid(){
-        for (int i = 0; i < 10; ++i) {
-            for (int j = 0; j < 10; ++j) {
-                GridArray[i][j] = 0;
+    // Initializes all grid spaces to empty
+    Grid() {
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 12; ++j) {
+                GridArray[i][j] = EMPTY;
+            }
+        }
+        // Place walls around the grid
+        PlaceWalls();
+        
+        // Randomly place batteries on the grid
+        PlaceBatteries(40);  // Assuming 40 batteries to be placed
+    }
+
+    void PlaceWalls(){
+        // Place walls on top and bottom rows
+        for (int i = 0; i < 12; ++i) {
+            GridArray[0][i] = WALL; // Top row
+            GridArray[11][i] = WALL; // Bottom row
+        }
+
+        // Place walls on left and right columns
+        for (int i = 1; i < 11; ++i) { // Start and end at 1 and 11 to avoid overwriting corners
+            GridArray[i][0] = WALL; // Left column
+            GridArray[i][11] = WALL; // Right column
+        }
+    }
+
+    void PlaceBatteries(int numBatteries) {
+        int placed = 0;
+        while (placed < numBatteries) {
+            int x = rand() % 10 + 1;
+            int y = rand() % 10 + 1;
+            if (GridArray[x][y] == EMPTY) {
+                GridArray[x][y] = BATTERY;
+                placed++;
             }
         }
     }
 
-    // Optional Task: Place obstacles around map
-    void InitializeObjects(int numObjects) {
-        srand(static_cast<unsigned int>(time(0)));
 
-        for (int i = 0; i < numObjects; ++i) {
-            int x, y;
-
-            do {
-                x = rand() % 10;
-                y = rand() % 10;
-            } while (GridArray[x][y] != 0 && IsAdjacent(x, y, 3));
-
-            GridArray[x][y] = 3;  // Object/Wall is placed in the cell
+    // Method to get the content of a cell
+    GridState GetCellContent(int x, int y) {
+        if (x >= 0 && x < 12 && y >= 0 && y < 12) {
+            return GridArray[x][y];
         }
+        return WALL;  // Return wall for out-of-bounds
     }
 
-
-    // Check adjacent cel other objects to prevent possible robot encapsulation
-    bool IsAdjacent(int x, int y, int valueToCheck) const {
-        for (int i = -1; i <= 1; ++i) {
-            for (int j = -1; j <= 1; ++j) {
-                if (i == 0 && j == 0) continue; // Skip the current cell
-
-                int newX = x + i;
-                int newY = y + j;
-
-                if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10 && GridArray[newX][newY] == valueToCheck) {
-                    return true; // Adjacent cell has the specified value
-                }
-            }
-        }
-        return false;
-    }
-
-    void InitializeRobots() {
-        srand(static_cast<unsigned int>(time(0)));
-        int x, y;
-
-        // Find an empty cell
-        do {
-            x = rand() % 10;
-            y = rand() % 10;
-        } while (GridArray[x][y] != 0);  // Ensure the cell is empty
-
-        GridArray[x][y] = 1;  // Place the robot in the empty cell
-    }
-
-    void InitializeBatteries(int numBatteries){
-        srand(static_cast<unsigned int>(time(0)));
-        int x, y;
-
-        for (int i = 0; i < numBatteries; i++) {
-        // Find an empty cell
-            do {
-                x = rand() % 10;
-                y = rand() % 10;
-            } while (GridArray[x][y] != 0);  // Ensure the cell is empty
-
-            GridArray[x][y] = 2;  // Place the robot in the empty cell
-        }
-    }
 
     void PlaceRobot() {
-        srand(static_cast<unsigned int>(time(0)));
         int x, y;
+
         do {
-            x = rand() % 10;
-            y = rand() % 10;
-        } while (GridArray[x][y] != 0);
+            // Generate random coordinates within the inner grid (excluding walls)
+            x = rand() % 10 + 1;
+            y = rand() % 10 + 1;
+        } while (GridArray[x][y] != EMPTY); // Repeat until an empty cell is found
 
-        GridArray[x][y] = 1;  // Place robot in cell
+        // Place the robot in the found empty cell
+        GridArray[x][y] = ROBOT;
     }
-
-// TEST CASE
-// void PlaceRobot() {
-//     srand(static_cast<unsigned int>(time(0)));
-//     int x, y;
-//     do {
-//         x = rand() % 10;
-//         y = rand() % 10;
-//     } while (GridArray[x][y] != 0);
-
-//     GridArray[x][y] = 1;  // Place robot in cell
-
-//     // Place a battery to the right of the robot, if within grid boundaries
-//     if (x + 1 < 10) {
-//         GridArray[x + 1][y] = 2;  // Place a battery
-//     }
-// }
 
 
     void RemoveRobot(int x, int y) {
-        if (x >= 0 && x < 10 && y >= 0 && y < 10) {
-            GridArray[x][y] = 0;
+        if (x >= 1 && x <= 10 && y >= 1 && y <= 10) { // Searches inner grid only
+            GridArray[x][y] = EMPTY;  
         }
     }
 
-    int GetContent(int x, int y) const {
-        if (x >= 0 && x < 10 && y >= 0 && y < 10) {
-            return GridArray[x][y];
-        } else {
-            // Return a value indicating an out-of-bounds cell
-            return -1;
-            }
+
+    void PlaceBattery() {
+        int x, y;
+
+        do {
+            // Generate random coordinates within the inner grid (excluding walls)
+            x = rand() % 10 + 1;
+            y = rand() % 10 + 1;
+        } while (GridArray[x][y] != EMPTY); // Repeat until an empty cell is found
+
+        // Place the battery in the found empty cell
+        GridArray[x][y] = BATTERY;
     }
 
 
-    // In the Grid class:
-    void ConsumeBattery(int x, int y) {
-        if (x >= 0 && x < 10 && y >= 0 && y < 10) {
-            GridArray[x][y] = 0; // Remove the battery from the grid
-        //DEBUG
-        cout << "Battery consumed at: (" << x << ", " << y << ")" << endl;
+    void RemoveBattery(int x, int y) {
+        if (x >= 1 && x <= 10 && y >= 1 && y <= 10) { // Searches inner grid only
+            GridArray[x][y] = EMPTY;  
         }
     }
+
 };
 
-
-class Robot {
-private:
-    // enum SensorStrength {NORMAL, DOUBLE}; // Meaning: Normal - Senors sees 1 space all around, DOUBLE - Sensors sees 2 spaces all around
-    enum SensorReading {CLEAR, OBJECT, BATTERY, IGNORE};
-    enum Movement {NORTH, SOUTH, EAST, WEST, RANDOM};
-    int x;
-    int y;
-    int Position; // Holds the current location of the robot
-    int Energy;
-    int TurnsSurvived;
-    int TotalEnergyHarvested = 0;
-    Movement decisionDirection;
-
-    // struct Gene {
-    //     SensorReading sensorReading;
-    //     SensorReading nextSensorReading;
-    //     Movement direction;
-    // };
-    
-// TEST CASE
-struct Gene {
-    SensorReading sensorReading;
-    SensorReading nextSensorReading;
-    Movement direction;
-    float randomnessFactor; // New gene trait for randomness
-};
-
-
-
-    struct SensorData {
-        SensorReading north;
-        SensorReading south;
-        SensorReading east;
-        SensorReading west;
-    };
-
-    Gene genes[16];
-    int currentPosition;
-    int energy;
-    int currentGeneIndex; 
-
-public:
-    Robot(){
-        x = rand() % 10;
-        y = rand() % 10;
-        for (int i = 0; i < 16; ++i) {
-            genes[i].sensorReading = static_cast<SensorReading>(rand() % 4);
-            genes[i].nextSensorReading = static_cast<SensorReading>(rand() % 4);
-
-            // Generate a random value between 0 and 4 (inclusive) for direction, including RANDOM
-            int directionIndex = std::rand() % 5;
-            genes[i].direction = static_cast<Movement>(directionIndex);
-
-            // TEST CASE
-            genes[i].randomnessFactor = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-        }
-
-        currentPosition = rand() % 10;
-        energy = 5;
-        currentGeneIndex = 0;
-    }
-
-    SensorData currentSensorData;
-
-    // Method to update sensors based on the grid
-void UpdateSensors(const Grid& grid) {
-    currentSensorData.north = static_cast<SensorReading>(grid.GetContent(x, y + 1));
-    currentSensorData.south = static_cast<SensorReading>(grid.GetContent(x, y - 1));
-    currentSensorData.east = static_cast<SensorReading>(grid.GetContent(x + 1, y));
-    currentSensorData.west = static_cast<SensorReading>(grid.GetContent(x - 1, y));
-}
-
-
-    // Method to make decisions based on sensors and genes
-    void MakeDecisions() {
-        // Check each direction and make a decision based on the gene's strategy
-        if (currentSensorData.north == genes[currentGeneIndex].sensorReading) {
-            decisionDirection = genes[currentGeneIndex].direction; // Use gene's direction if sensor reading matches
-        } else if (currentSensorData.south == genes[currentGeneIndex].sensorReading) {
-            decisionDirection = genes[currentGeneIndex].direction;
-        } else if (currentSensorData.east == genes[currentGeneIndex].sensorReading) {
-            decisionDirection = genes[currentGeneIndex].direction;
-        } else if (currentSensorData.west == genes[currentGeneIndex].sensorReading) {
-            decisionDirection = genes[currentGeneIndex].direction;
-        } else {
-            // If no sensor data matches the current gene, choose a random direction
-            decisionDirection = static_cast<Movement>(rand() % 4);
-        }
-
-        // TEST CASE
-        float randomValue = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-        if (randomValue < genes[currentGeneIndex].randomnessFactor) {
-            decisionDirection = static_cast<Movement>(rand() % 5); // Includes RANDOM direction
-        }
-
-        // Increment the gene index for the next decision
-        currentGeneIndex = (currentGeneIndex + 1) % 16; 
-    }
-
-
-
-    // Method to move the robot and update position and energy
-    void Move() {
-        // Modify this method to use the direction decided in MakeDecisions
-        // For example, if the decision is to move north:
-        if (decisionDirection == NORTH) {
-            y = (y < 9) ? y + 1 : y; // Ensuring the robot doesn't go out of bounds
-        } else if (decisionDirection == SOUTH) {
-            y = (y > 0) ? y - 1: y;
-        } else if (decisionDirection == EAST) {
-            x = (x < 9) ? x + 1 : x;
-        } else if (decisionDirection == WEST) {
-            x = (x > 0) ? x - 1 : x;
-        }
-
-        energy -= 1; // 1 energy depleted with every move
-
-    // //DEBUG
-    // cout << "Robot moved to (" << x << ", " << y << ") | Energy after move: " << energy << endl;
-
-    }
-
-    // Method to handle energy depletion and restoration
-    void HandleEnergy(Grid& grid) {
-        int cellContent = grid.GetContent(x, y);
-
-        if (cellContent == 2) {
-            // DEBUG
-            cout << "Robot found battery at (" << x << ", " << y << ")" << endl;
-            energy += 5;
-            TotalEnergyHarvested += 5;
-            TurnsSurvived += 5 * 5; // Update TurnsSurvived based on energy harvested
-            grid.ConsumeBattery(x, y);
-        }
-        // DEBUG
-        cout << "Handled energy at (" << x << ", " << y << ") | Energy now: " << energy << endl;
-    }
-
-
-    void ResetEnergy() {
-        // energy = 5;
-        energy = 50; //FIX ME TESTING PURPOSES ONLY
-    }
-
-    void RunRound(Grid& grid, int robotNumber) {
-        ResetEnergy();
-        TotalEnergyHarvested = 0; // Reset total energy harvested
-        TurnsSurvived = 0;        // Reset turns survived
-        // DEBUG
-        cout << "Robot " << robotNumber << " starting at (" << x << ", " << y << ") with energy " << energy << endl;
-
-        while (energy > 0) {
-            UpdateSensors(grid);
-            MakeDecisions();
-            Move();
-            HandleEnergy(grid); // Energy handling might change TurnsSurvived
-            TurnsSurvived++;
-        }
-
-        cout << "Robot " << robotNumber << " | Total Harvested: " << TotalEnergyHarvested 
-            << " | Turns Survived: " << TurnsSurvived << endl;
-    }
-
-
-    int GetTotalEnergyHarvested() const {
-        return TotalEnergyHarvested;
-    }
-
-    int GetTurnsSurvived() const {
-        return TurnsSurvived;
-    }    
-
-    int GetEnergy() const {
-        return energy;
-    }
-
-    int GetX() const {
-        return x;
-    }
-
-    int GetY() const {
-        return y;
-    }
-};
-
-
-class Simulation {
-private:
-    Robot population[200];
-    int generation;
-    FitnessData generationFitness[10]; // Using the FitnessData struct to store fitness data
-
-public:
-    Simulation() : generation(0) {
-        for (auto &robot : population) {
-            robot = Robot();
-        }
-    }
-
-    void RunGeneration(Grid& grid) {
-        double totalFitness = 0;
-
-        for (int i = 0; i < 200; ++i) {
-            grid.PlaceRobot();
-            population[i].RunRound(grid, i + 1);
-            
-            // Get the robot's position for removal
-            int x = population[i].GetX();
-            int y = population[i].GetY();
-            grid.RemoveRobot(x, y);
-
-            totalFitness += static_cast<double>(population[i].GetTotalEnergyHarvested()) / population[i].GetTurnsSurvived();
-        }
-
-        generationFitness[generation].averageFitness = totalFitness / 200;
-        ++generation;
-    }
-
-    void PrintAllFitnessData() const {
-        cout << "\nGeneration Fitness Data:\n";
-        for (int i = 0; i < generation; ++i) {
-            cout << "Generation " << (i + 1) << ": Average Fitness = " << generationFitness[i].averageFitness << endl;
-        }
-    }
-
-    void RunSimulation(Grid& grid, int numGenerations) {
-        for (int gen = 0; gen < numGenerations; ++gen) {
-            RunGeneration(grid);
-        }
-        PrintAllFitnessData();
-    }
-};
 
 
 int main() {
+    srand(static_cast<unsigned int>(time(0)));
     Grid grid;
-    grid.InitializeObjects(20);
-    grid.InitializeRobots();
-    grid.InitializeBatteries(40);
 
-    Simulation simulation;
-    simulation.RunSimulation(grid, 10);
+    // Further testing and implementation...
 
     return 0;
 }
